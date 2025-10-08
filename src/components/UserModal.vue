6d5cea9419f8740ca1220144e29117fa948c9aa9
@vaneca06-is <vaneca06@gmail.com>
2025-10-08 09:14:14 -0600

    files

---
+++ src/components/UserModal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="modal">
+        <div class="modal-content">
+            <h1>{{ user.name }}</h1>
+            <p>EMAIL: {{ user.email }}</p>
+            <p>USER NAME: {{ user.username }}</p>
+            <p>ADDRESS: {{ user.address.street }}, {{ user.address.city }}</p>
+            <p>ID: {{user.id  }}</p>
+            <p>PHONE: {{ user.phone }}</p>
+            <p>COMPANY: {{ user.company.name }}</p>
+            <button @click="$emit('modalFalse')">Close</button>
+        </div>
+
+    </div>
+</template>
+<script setup>
+import { defineProps, ref } from "vue";
+
+const props = defineProps({
+    prop_userSelected: Object,
+})
+const user = ref(props.prop_userSelected);
+</script>
+
+<style>
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-content {
+    background-color: white;
+    width: 400px;
+    border-radius: 10px;
+    padding: 20px;
+}
+</style>
---
+++ src/components/UserTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Npmbre</th>
+                <th>Email</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="user in props.userTable" :key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+                <td>
+                    <button @click="$emit('show-details', user)">Ver</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+    userTable: Array,
+})
+
+//const emit = defineEmits(["show-details"]); // SAME TO USE "$emit('show-details', user)
+</script>
